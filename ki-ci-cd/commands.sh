4c1f737efa012295d42058e5c19bf0e0c012d97e
Kostiantyn Iarovyi <kyarovoy@gmail.com>
2018-06-10 02:06:42 -0400

    Added TODO

--- ki-ci-cd/commands.sh
+++ ki-ci-cd/commands.sh
@@ -1,9 +1,15 @@
 oc new-project ki-ci-cd
 
+# TODO: global vars
+# TODO: resource limits as template params for all templates
+
 # Jenkins
 oc new-app jenkins-persistent -p ENABLE_OAUTH=true -p MEMORY_LIMIT=2Gi -p VOLUME_CAPACITY=4Gi
 
 # Gogs
+# TODO: sed for XML manipulation
+# TODO: move gogs template to my repo
+
 oc new-app -f http://bit.ly/openshift-gogs-persistent-template -p HOSTNAME=gogs-ki-ci-cd.apps.na37.openshift.opentlc.com
 # Go to Gogs, create account (gogs_admin:123456) and new org 'MitziCom'
 cd /tmp
@@ -16,6 +22,8 @@ git remote add private http://gogs_admin:123456@gogs-ki-ci-cd.apps.na37.openshif
 git push private master
 
 # Nexus
+# TODO: add Docker registry to template
+# TODO: one liner for curl
 oc new-app -f https://github.com/kyarovoy/openshift_advdev_homework/blob/master/ki-ci-cd/nexus-persistent-template.yml -p HOSTNAME=nexus3-ki-ci-cd.apps.na37.openshift.opentlc.com
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
@@ -25,5 +33,6 @@ oc expose dc nexus3 --port=5000 --name=nexus-registry
 oc create route edge nexus-registry --service=nexus-registry --port=5000
 
 # SonarQube
+# TODO: combine
 oc new-app --template=postgresql-persistent -p POSTGRESQL_USER=sonar -p POSTGRESQL_PASSWORD=sonar -p POSTGRESQL_DATABASE=sonar -p VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
 oc new-app -f https://github.com/kyarovoy/openshift_advdev_homework/blob/master/ki-ci-cd/sonarqube-persistent-template.yml -p HOSTNAME -p SONARQUBE_JDBC_USERNAME=sonar -p SONARQUBE_JDBC_PASSWORD=sonar -p SONARQUBE_JDBC_DBNAME=sonar
