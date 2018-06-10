34b3c8bdd8ab21471f43f394f33be4af52fa6269
Kostiantyn Iarovyi <kyarovoy@gmail.com>
2018-06-10 01:23:45 -0400

    Fixed nexus persistent template

--- ki-ci-cd/commands.sh
+++ ki-ci-cd/commands.sh
@@ -16,17 +16,7 @@ git remote add private http://gogs_admin:123456@gogs-ki-ci-cd.apps.na37.openshif
 git push private master
 
 # Nexus
-oc new-app sonatype/nexus3:latest
-oc expose svc nexus3
-oc rollout pause dc nexus3
-oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
-oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
-oc create -f nexus_pvc.yml
-oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
-oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
-oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
-oc rollout resume dc nexus3
-# Wait until Nexus is available
+oc new-app -f https://github.com/kyarovoy/openshift_advdev_homework/blob/master/ki-ci-cd/nexus-persistent-template.yml -p HOSTNAME=nexus3-ki-ci-cd.apps.na37.openshift.opentlc.com
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
