29cf8091ebc77a647575ab8ea3d97947491813ed
Kostiantyn Iarovyi <kyarovoy@gmail.com>
2018-09-11 16:14:17 -0400

    Merged PGSQL and Sonar in 1 template and reworked it a little bit

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -9,6 +9,7 @@ fi
 GUID=$1
 echo "Setting up Sonarqube in project $GUID-sonarqube"
 
-oc project $GUID-sonarqube
-oc new-app --template=postgresql-persistent -p POSTGRESQL_USER=sonar -p POSTGRESQL_PASSWORD=sonar -p POSTGRESQL_DATABASE=sonar -p VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
-oc new-app -f ../templates/sonarqube.yaml -p HOSTNAME -p SONARQUBE_JDBC_USERNAME=sonar -p SONARQUBE_JDBC_PASSWORD=sonar -p SONARQUBE_JDBC_DBNAME=sonar
+oc -n $GUID-sonarqube new-app -f ../templates/sonarqube.yaml
+oc -n $GUID-sonarqube rollout status dc/sonarqube -w
+
+
